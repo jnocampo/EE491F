9fbf9c9eded8383077a07fe24d6c7f703f50bfe2
Jessica Ocampo <jnocampo@hawaii.edu>
2021-06-09 21:13:00 -1000

    fixing minor syntax for jQuery assignment

--- jQuery-exercise/main.js
+++ jQuery-exercise/main.js
@@ -7,10 +7,10 @@
 $(function() {
     $('#image-hover').hover(function() {
         $(this).width(1.25 * $(this).width());
-        $(this).height(1.25 * $(this).hegiht());
+        $(this).height(1.25 * $(this).height());
     }, function(){
         $(this).width(0.8 * $(this).width());
-        $(this).height(0.8 * $(this).hegiht());
+        $(this).height(0.8 * $(this).height());
     });
 });
 
